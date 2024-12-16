390b64af60e0c4d07486b430633c7fbd0e666874
mansi <solankimansi2325@gmail.com>
2024-12-17 01:03:10 +0530

    Updated server-side code

--- Server/index.js
+++ Server/index.js
@@ -21,8 +21,8 @@ const server = http.createServer(app);
 
 // Middleware
 app.use(cors());
-app.use(express.json({ limit: "50mb", extended: true }));
-app.use(express.urlencoded({ limit: "50mb", extended: true }));
+app.use(express.json({ limit: "100mb", extended: true }));
+app.use(express.urlencoded({ limit: "100mb", extended: true }));
 app.use(bodyParser.json());
 app.use("/uploads", express.static(path.join("uploads")));
 
