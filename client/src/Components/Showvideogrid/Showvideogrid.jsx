449094d8a90d26edda038ecad70801a96c057ef7
creativegithub <140626864+creativegithub@users.noreply.github.com>
2024-12-17 21:42:19 +0530

    Update Showvideogrid

--- client/src/Components/Showvideogrid/Showvideogrid.jsx
+++ client/src/Components/Showvideogrid/Showvideogrid.jsx
@@ -5,7 +5,7 @@ import Showvideo from "../Showvideo/Showvideo";
 function Showvideogrid({ vid }) {
   return (
     <div className="Container_ShowVideoGrid">
-      {vid?.reverse().map((vi) => {
+      {vid?.map((vi) => {
         return (
           <div key={vi._id} className="video_box_app">
             <Showvideo vid={vi} />
